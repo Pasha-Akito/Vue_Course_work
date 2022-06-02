c660d1e8cfc8630992c71f296731287d13e53272
Pasha-Akito <1masterpasha@gmail.com>
2022-06-02 14:53:06 +0100

    adding vue to plain js

--- starting_project/app.js
+++ starting_project/app.js
@@ -1,14 +1,30 @@
-const buttonEle = document.querySelector('button');
-const inputEle = document.querySelector('input');
-const ListEle = document.querySelector('ul');
+Vue.createApp({
+    data() {
+        return {
+            goals: [],
+            enteredValue: ''
+        }
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue)
+            this.enteredValue = '';
+        }
+    }
+}).mount('#app');
 
-function addGoal() {
-    const enteredValue = inputEle.value;
-    const listItemEle = document.createElement('li');
-    listItemEle.textContent = enteredValue;
-    ListEle.appendChild(listItemEle);
-    inputEle.value = '';
-}
 
-buttonEle.addEventListener('click', addGoal);
+// const buttonEle = document.querySelector('button');
+// const inputEle = document.querySelector('input');
+// const ListEle = document.querySelector('ul');
+
+// function addGoal() {
+//     const enteredValue = inputEle.value;
+//     const listItemEle = document.createElement('li');
+//     listItemEle.textContent = enteredValue;
+//     ListEle.appendChild(listItemEle);
+//     inputEle.value = '';
+// }
+
+// buttonEle.addEventListener('click', addGoal);
 
