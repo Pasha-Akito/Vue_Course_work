31166d89abde15dcc389ecf98c81d8139e309a75
Pasha-Akito <1masterpasha@gmail.com>
2022-06-07 23:26:22 +0100

    watcher properties

--- basics-05-using-the-native-event-object/app.js
+++ basics-05-using-the-native-event-object/app.js
@@ -2,13 +2,39 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      lastName: '',
+      //fullName: ''
     };
+  },
+  watch: {
+    counter(value) {
+      if (value > 50) {
+        this.counter = 0;
+      }
+    }
+    // name(value) {
+    //   if (value === '') {
+    //     this.fullName = '';
+    //   } else {
+    //     this.fullName = value + ' ' + this.lastName;
+    //   }
+    // },
+    // lastName(value) {
+    //   if (value === '') {
+    //     this.fullName = '';
+    //   } else {
+    //     this.fullName = this.name + ' ' + value;
+    //   }
+    // }
   },
   computed: {
     fullName() {
-      if (this.name === '') return '';
-      return this.name + ' ' + 'Antonov';
+      if (this.name === '' || this.lastName === '') {
+        return '';
+      } else {
+        return this.name + ' ' + this.lastName;
+      }
     }
   },
   methods: {
@@ -27,6 +53,7 @@ const app = Vue.createApp({
     },
     resetInput() {
       this.name = '';
+      this.lastName = '';
     }
   }
 });
