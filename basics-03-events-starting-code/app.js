00e9e05be65d4c7f30522edae25605869e573fcd
Pasha-Akito <1masterpasha@gmail.com>
2022-06-07 14:52:50 +0100

    Event modifiers and v-once

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -1,18 +1,25 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 0,
-      name: ''
+      counter: 10,
+      name: '',
+      confirmedName: ''
     };
   },
   methods: {
+    confirmInput(){
+      this.confirmedName = this.name;
+    },
+    submitForm() {
+      alert('Submitted');
+    },
     add(num) {
       this.counter = this.counter + num;
     },
-    subtract (num) {
+    subtract(num) {
       this.counter = this.counter - num;
     },
-    setName (event, lastname) {
+    setName(event, lastname) {
       this.name = event.target.value + " " + lastname;
     }
   }
